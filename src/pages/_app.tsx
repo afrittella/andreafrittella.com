92850b93ff9866f827db367411e7fa3a8a09ae83
afrittella <me.liveset@gmail.com>
2024-01-30 17:19:41 +0100

    Upgrade Vercel Analytics to SpeedInsights.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import 'styles/globals.css'
-import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default MyApp
